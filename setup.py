c12084853741fb63564e31ea5e02e09ad5ea8cb7
cammm <81903+cammm@users.noreply.github.com>
2018-10-19 15:45:56 -0700

    Empty project checkin

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+  name         = 'deargui',
+  description  = 'Python bindings for Dear ImGui',
+  version      = '0.1.0',
+  url          = 'http://github.com/cammm/deargui',
+  license      = 'MIT',
+  author       = 'cammm',
+  packages     = setuptools.find_packages(),
+)
